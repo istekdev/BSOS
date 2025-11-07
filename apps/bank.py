f293ac80c2c8952d0a13b9aa907c22ac2d077983
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-07 09:34:48 -0600

    Update bank.py

--- apps/bank.py
+++ apps/bank.py
@@ -3,10 +3,74 @@ import system
 import json
 import time
 
-# Tomorrow
+def withColor():
+  print(colored("BSOS Bank", "white", attrs=["bold"]))
+  print("")
+  print(colored("[B] - View Balance", "white", attrs=["bold"]))
+  input = input(colored(">> ", "white", attrs=["bold"]))
+  if input.upper() in ["B", "[B]"]:
+    with open("config.json", "r") as r:
+      view = json.load(r)
+    print(colored(f"Current Balance: ${str(view["apps"]["bank"]["balance"])}", "white", attrs=["bold"]))
+    print("")
+    print(colored("[X] - Exit", "white", attrs=["bold"]))
+    option = input(colored(">> ", "white", attrs=["bold"]))
+    if option.upper() in ["X", "[X]"]:
+      system.clear()
+      withColor()
+    else:
+      print(colored("Error - Function Dosen't Exist", "red", attrs=["bold"]))
+      time.sleep(1)
+      system.clear()
+      withColor()
+  else:
+    print(colored("Error - Function Dosen't Exist", "red", attrs=["bold"]))
+    time.sleep(1)
+    system.clear()
+    withColor()
+
+def nonColor():
+  print("BSOS Bank")
+  print("")
+  print("[B] - View Balance")
+  input = input(">> ")
+  if input.upper() in ["B", "[B]"]:
+    with open("config.json", "r") as r:
+      view = json.load(r)
+    print(f"Current Balance: ${str(view["apps"]["bank"]["balance"])}")
+    print("")
+    print("[X] - Exit")
+    option = input(">> ")
+    if option.upper() in ["X", "[X]"]:
+      system.clear()
+      withColor()
+    else:
+      print("Error - Function Dosen't Exist")
+      time.sleep(1)
+      system.clear()
+      withColor()
+  else:
+    print("Error - Function Dosen't Exist")
+    time.sleep(1)
+    system.clear()
+    withColor()
 
 with open("./config.json", "r") as r:
   config = json.load(r)
+
+if config["config"]["colors"] == True:
+  print(colored("Verifying...", "yellow", attrs=["bold"]))
+else:
+  print("Verifying...")
+
+if config["apps"]["bank"]["balance"] == 0:
+  config["apps"]["bank"]["balance"] == 1000
+  with open("config.json", "w") as w:
+    json.dump(config, w, indent=4)
+  if config["config"]["colors"] == True:
+    print(colored("Welcome! You Have Been Given $1,000", "white", attrs=["bold"]))
+  else:
+    print("Welcome! You Have Been Given $1,000")
 
 if config["config"]["colors"] == True:
   withColor()
