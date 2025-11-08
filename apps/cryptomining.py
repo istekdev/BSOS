0ecb1d21e7f9b3607a7ca082146cf01787f8a7a9
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-08 16:28:27 -0600

    Update cryptomining.py

--- apps/cryptomining.py
+++ apps/cryptomining.py
@@ -1,4 +1,3 @@
-from termcolor import colored
 from security import hashing
 from hashlib import sha256
 import system
@@ -33,21 +32,21 @@ def menu():
   with open("./config.json", "r") as r:
     config = json.load(r)
   if config["config"]["colors"] == True:
-    print(colored(menuTitle + "\n\n", "white", attrs=["bold"]))
-    input = input(colored(">> ", "white", attrs=["bold"]))
+    print(system.coloring(menuTitle + "\n\n", "white"))
+    input = input(system.coloring(">> ", "white"))
     if input.upper() in ["S", "[S]"]:
       system.clear()
       while nonce < max:
         nonce += 1
         bHash = sha256(sha256(block()[0].to_bytes(4, "big") + block()[1].to_bytes(3, "big") + block()[2] + bytes.fromhex(block()[3]) + block()[4].encode("utf-8") + nonce.to_bytes(32, "big")).digest()).hexdigest()
         if config["config"]["colors"] == True:
-          print(colored(f"Mining - Current Hash: {bHash}", "yellow", attrs=["bold"]))
+          print(system.coloring(f"Mining - Current Hash: {bHash}", "yellow"))
         else:
           print(f"Mining - Current Hash: {bHash}")
       
         if int(bHash, 16) <= target:
           if config["config"]["colors"] == True:
-            print(colored("Block Has Been Mined! You Earned $2,500!", "green", attrs=["bold"]))
+            print(system.coloring("Block Has Been Mined! You Earned $2,500!", "green"))
           else:
             print("Block Has Been Mined! You Earned $2,500!")
             Balance = config["apps"]["bank"]["balance"] + 2500
@@ -63,7 +62,7 @@ def menu():
       system.clear()
       system.home()
     else:
-      print(colored("Error - Function Does Not Exist", "red", attrs=["bold"]))
+      print(system.coloring("Error - Function Does Not Exist", "red"))
       time.sleep(3)
       system.clear()
       system.home()
@@ -76,13 +75,13 @@ def menu():
         nonce += 1
         bHash = sha256(sha256(block()[0].to_bytes(4, "big") + block()[1].to_bytes(3, "big") + block()[2] + bytes.fromhex(block()[3]) + block()[4].encode("utf-8") + nonce.to_bytes(32, "big")).digest()).hexdigest()
         if config["config"]["colors"] == True:
-          print(colored(f"Mining - Current Hash: {bHash}", "yellow", attrs=["bold"]))
+          print(system.coloring(f"Mining - Current Hash: {bHash}", "yellow"))
         else:
           print(f"Mining - Current Hash: {bHash}")
       
         if int(bHash, 16) <= target:
           if config["config"]["colors"] == True:
-            print(colored("Block Has Been Mined! You Earned $2,500!", "green", attrs=["bold"]))
+            print(system.coloring("Block Has Been Mined! You Earned $2,500!", "green"))
           else:
             print("Block Has Been Mined! You Earned $2,500!")
             Balance = config["apps"]["bank"]["balance"] + 2500
