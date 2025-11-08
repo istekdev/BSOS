8ebd584411eb95fcb1556a067cb5d8ebb200c3e1
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-08 16:17:48 -0600

    Update hashing.py

--- security/hashing.py
+++ security/hashing.py
@@ -14,3 +14,17 @@ def start():
   config["configHash"] = sha256(sha256(static).digest()).hexdigest()
   with open("./config.json", "w") as w:
     json.dump(config, w, indent=4)
+
+def verify():
+  nonce = None
+  verified = False
+  with open("./config.json", "r") as r:
+    config = json.load(r)
+  if config["dynamic"]["nonce"] == None:
+    nonce = "None".encode("utf-8")
+  else:
+    nonce = bytes.fromhex(config["dynamic"]["nonce"])
+  static = ((config["socials"]["developer"]).encode("utf-8") + (config["socials"]["github"]).encode("utf-8") + str(config["socials"]["created"]).encode("utf-8") + str(config["config"]["colors"]).encode("utf-8") + nonce + str(config["apps"]["bank"]["balance"]).encode("utf-8"))
+  if config["configHash"] == sha256(sha256(static).digest()).hexdigest():
+    verified = True
+  return verified
