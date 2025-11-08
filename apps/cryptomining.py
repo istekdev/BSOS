b74ae76a3d297b067d44bd621f4d0784241c1b6b
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-08 16:24:21 -0600

    Update cryptomining.py

--- apps/cryptomining.py
+++ apps/cryptomining.py
@@ -27,43 +27,38 @@ def block():
   blockHash = sha256(sha256(version.to_bytes(4, "big") + height.to_bytes(3, "big") + merkleRoot + bytes.fromhex(Target) + timestamp.encode("utf-8") + nonce.to_bytes(32, "big")).digest()).hexdigest()
   return version, height, merkleRoot, Target, timestamp, nonce, blockHash
 
-def mine():
-  with open("./config.json", "r") as r:
-    config = json.load(r)
+def menu():
   max = (2**32) - 1
   nonce = 0
-  while nonce < max:
-    nonce += 1
-    bHash = sha256(sha256(block()[0].to_bytes(4, "big") + block()[1].to_bytes(3, "big") + block()[2] + bytes.fromhex(block()[3]) + block()[4].encode("utf-8") + nonce.to_bytes(32, "big")).digest()).hexdigest()
-    if config["config"]["colors"] == True:
-      print(colored(f"Mining - Current Hash: {bHash}", "yellow", attrs=["bold"]))
-    else:
-      print(f"Mining - Current Hash: {bHash}")
-      
-    if int(bHash, 16) <= target:
-      if config["config"]["colors"] == True:
-        print(colored("Block Has Been Mined! You Earned $2,500!", "green", attrs=["bold"]))
-      else:
-        print("Block Has Been Mined! You Earned $2,500!")
-        Balance = config["apps"]["bank"]["balance"] + 2500
-      with open("./config.json", "w") as w:
-        json.dump(Balance, w, indent=4)
-      hashing.start()
-        
-      time.sleep(1)
-      break
-      system.clear()
-      system.home()
-
-def menu():
   with open("./config.json", "r") as r:
-    ver = json.load(r)
-  if ver["config"]["colors"] == True:
+    config = json.load(r)
+  if config["config"]["colors"] == True:
     print(colored(menuTitle + "\n\n", "white", attrs=["bold"]))
     input = input(colored(">> ", "white", attrs=["bold"]))
     if input.upper() in ["S", "[S]"]:
       system.clear()
-      mine()
+      while nonce < max:
+        nonce += 1
+        bHash = sha256(sha256(block()[0].to_bytes(4, "big") + block()[1].to_bytes(3, "big") + block()[2] + bytes.fromhex(block()[3]) + block()[4].encode("utf-8") + nonce.to_bytes(32, "big")).digest()).hexdigest()
+        if config["config"]["colors"] == True:
+          print(colored(f"Mining - Current Hash: {bHash}", "yellow", attrs=["bold"]))
+        else:
+          print(f"Mining - Current Hash: {bHash}")
+      
+        if int(bHash, 16) <= target:
+          if config["config"]["colors"] == True:
+            print(colored("Block Has Been Mined! You Earned $2,500!", "green", attrs=["bold"]))
+          else:
+            print("Block Has Been Mined! You Earned $2,500!")
+            Balance = config["apps"]["bank"]["balance"] + 2500
+          with open("./config.json", "w") as w:
+            json.dump(Balance, w, indent=4)
+          hashing.start()
+        
+          time.sleep(1)
+          break
+          system.clear()
+          menu()
     elif input.upper() in ["X", "[X]"]:
       system.clear()
       system.home()
@@ -77,7 +72,28 @@ def menu():
     input = input(">> ")
     if input.upper() in ["S", "[S]"]:
       system.clear()
-      mine()
+      while nonce < max:
+        nonce += 1
+        bHash = sha256(sha256(block()[0].to_bytes(4, "big") + block()[1].to_bytes(3, "big") + block()[2] + bytes.fromhex(block()[3]) + block()[4].encode("utf-8") + nonce.to_bytes(32, "big")).digest()).hexdigest()
+        if config["config"]["colors"] == True:
+          print(colored(f"Mining - Current Hash: {bHash}", "yellow", attrs=["bold"]))
+        else:
+          print(f"Mining - Current Hash: {bHash}")
+      
+        if int(bHash, 16) <= target:
+          if config["config"]["colors"] == True:
+            print(colored("Block Has Been Mined! You Earned $2,500!", "green", attrs=["bold"]))
+          else:
+            print("Block Has Been Mined! You Earned $2,500!")
+            Balance = config["apps"]["bank"]["balance"] + 2500
+          with open("./config.json", "w") as w:
+            json.dump(Balance, w, indent=4)
+          hashing.start()
+        
+          time.sleep(1)
+          break
+          system.clear()
+          menu()
     elif input.upper() in ["X", "[X]"]:
       system.clear()
       system.home()
@@ -85,5 +101,5 @@ def menu():
       print("Error - Function Does Not Exist")
       time.sleep(3)
       system.clear()
-      system.home()
+      menu()
   
