b36b2334348d6857148c6d96ae1b13366070625d
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-08 20:54:25 -0600

    Update hashing.py

--- security/hashing.py
+++ security/hashing.py
@@ -1,16 +1,15 @@
 from hashlib import sha256
 import json
+import os
+
+with open("./config.json", "r") as r:
+  config = json.load(r)
+static = ((config["socials"]["developer"]).encode("utf-8") + (config["socials"]["github"]).encode("utf-8") + str(config["socials"]["created"]).encode("utf-8") + str(config["config"]["colors"]).encode("utf-8") + (config["dynamic"]["user"]).encode("utf-8") + bytes.fromhex(config["dynamic"]["nonce"] + str(config["apps"]["bank"]["balance"]).encode("utf-8"))
 
 def start():
-  nonce = None
-  with open("./config.json", "r") as r:
-    config = json.load(r)
   if config["dynamic"]["nonce"] == None:
-    nonce = "None".encode("utf-8")
-  else:
-    nonce = bytes.fromhex(config["dynamic"]["nonce"])
-    
-  static = ((config["socials"]["developer"]).encode("utf-8") + (config["socials"]["github"]).encode("utf-8") + str(config["socials"]["created"]).encode("utf-8") + str(config["config"]["colors"]).encode("utf-8") + nonce + str(config["apps"]["bank"]["balance"]).encode("utf-8"))
+    nonce = os.urandom(10)
+    config["dynamic"]["nonce"] = nonce.hex()
   config["configHash"] = sha256(sha256(static).digest()).hexdigest()
   with open("./config.json", "w") as w:
     json.dump(config, w, indent=4)
@@ -18,13 +17,6 @@ def start():
 def verify():
   nonce = None
   verified = False
-  with open("./config.json", "r") as r:
-    config = json.load(r)
-  if config["dynamic"]["nonce"] == None:
-    nonce = "None".encode("utf-8")
-  else:
-    nonce = bytes.fromhex(config["dynamic"]["nonce"])
-  static = ((config["socials"]["developer"]).encode("utf-8") + (config["socials"]["github"]).encode("utf-8") + str(config["socials"]["created"]).encode("utf-8") + str(config["config"]["colors"]).encode("utf-8") + nonce + str(config["apps"]["bank"]["balance"]).encode("utf-8"))
   if config["configHash"] == sha256(sha256(static).digest()).hexdigest():
     verified = True
   return verified
