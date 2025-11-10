6434c06c8e118406ffffb98e5ab419e573fd53b0
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-09 20:18:58 -0600

    Create aes.py

---
+++ security/aes.py
@@ -0,0 +1,45 @@
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from hashlib import pbkdf2_hmac
+import ephemeral
+import hashing
+import system
+import json
+import time
+import os
+
+with open("config.json", "r") as r:
+  config = json.load(r)
+
+salt = bytes.fromhex(config["dynamic"]["salt"])
+nonce = bytes.fromhex(config["dynamic"]["nonce"])
+
+def encrypt():
+  if hashing.verify() == True:
+    if config["dynamic"]["salt"] == None:
+      config["dynamic"]["salt"] = hex(os.urandom(16))
+      with open("config.json", "w") as w:
+        json.dump(config, w, indent=4)
+      hashing.start()
+      password = (ephemeral.password).encode("utf-8")
+      aes = AESGCM(pbkdf2_hmac("sha256", password, salt, 200_000, dklen=32))
+      config["apps"]["bank"]["balance"] = aes.encrypt(nonce, config["apps"]["bank"]["balance"], None)
+      with open("config.json", "w") as w:
+        json.dump(config, w, indent=4)
+      ephemeral.wipe()
+      aes = None
+      return "LogOut", aes
+    else:
+      return False
+
+def decrypt():
+  if hashing.verify() == True:
+    password = (ephemeral.password).encode("utf-8")
+    aes = AESGCM(pbkdf2_hmac("sha256", password, salt, 200_000, dklen=32))
+    config["apps"]["bank"]["balance"] = aes.decrypt(nonce, config["apps"]["bank"]["balance"], None)
+    with open("config.json", "w") as w:
+      json.dump(config, w, indent=4)
+    ephemeral.wipe()
+    aes = None
+    return True
+  else:
+    return False
