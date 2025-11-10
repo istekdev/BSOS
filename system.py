92b546af80cc8e897f90d90ee23d0bad7be447d6
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-09 23:08:48 -0600

    Update system.py

--- system.py
+++ system.py
@@ -2,11 +2,3 @@ import os
 
 def clear():
   os.system('cls' if os.name == 'nt' else 'clear')
-
-def colors(message, color, type):
-  if type.lower() == "input":
-    return input(colored(message, color, attrs=["bold"]))
-  elif type.lower() == "print":
-    return print(colored(message, color, attrs=["bold"]))
-  else:
-    return None
