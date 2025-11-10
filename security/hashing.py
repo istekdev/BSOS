a552b2d8bb540a4e53f3f30a7a0eb752a374b187
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-10 08:32:16 -0600

    Update hashing.py

--- security/hashing.py
+++ security/hashing.py
@@ -5,10 +5,12 @@ import os
 
 with open("config.json", "r") as r:
   config = json.load(r)
+
+timestamp = ""
 if config["dynamic"]["timestamp"] == None:
-  config["dynamic"]["timestamp"] = "null"
-  with open("config.json", "w") as w:
-    json.dump(config, w, indent=4)
+  timestamp = "null"
+else:
+  timestamp = config["dynamic"]["timestamp"]
 static = ((config["socials"]["developer"]).encode("utf-8") + (config["socials"]["github"]).encode("utf-8") + str(config["socials"]["created"]).encode("utf-8") + str(config["config"]["version"]).encode("utf-8") + str(config["config"]["colors"]).encode("utf-8") + (config["dynamic"]["user"]).encode("utf-8") + str(config["dynamic"]["timestamp"]).encode("utf-8") + bytes.fromhex(config["dynamic"]["salt"]) + bytes.fromhex(config["dynamic"]["nonce"]) + str(config["apps"]["bank"]["balance"]).encode("utf-8"))
 
 def start():
