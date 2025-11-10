65755ffb8a96d76c58c7d196aa6e818196ed2b17
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-09 19:40:02 -0600

    Update system.py

--- system.py
+++ system.py
@@ -2,13 +2,12 @@ import core
 import json
 import os
 
+global config
+with open("config.json", "r") as r:
+  config = json.load(r)
+
 def clear():
   os.system('cls' if os.name == 'nt' else 'clear')
 
 def home():
   core.menu()
-
-def config():
-  global config
-  with open("config.json", "r") as r:
-  config = json.load(r)
