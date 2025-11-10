3ad681a6887112b768f86a112282ec3fd6e69175
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-09 19:40:34 -0600

    Update hashing.py

--- security/hashing.py
+++ security/hashing.py
@@ -1,21 +1,20 @@
 from hashlib import sha256
+import system
 import json
 import os
 
-with open("./config.json", "r") as r:
-  config = json.load(r)
 static = ((config["socials"]["developer"]).encode("utf-8") + (config["socials"]["github"]).encode("utf-8") + str(config["socials"]["created"]).encode("utf-8") + str(config["config"]["version"]).encode("utf-8") + str(config["config"]["colors"]).encode("utf-8") + (config["dynamic"]["user"]).encode("utf-8") + bytes.fromhex(config["dynamic"]["nonce"]) + str(config["apps"]["bank"]["balance"]).encode("utf-8"))
 
 def start():
-  if config["dynamic"]["nonce"] == None:
+  if system.config["dynamic"]["nonce"] == None:
     nonce = os.urandom(10)
-    config["dynamic"]["nonce"] = nonce.hex()
-  config["configHash"] = sha256(sha256(static).digest()).hexdigest()
-  with open("./config.json", "w") as w:
-    json.dump(config, w, indent=4)
+    system.config["dynamic"]["nonce"] = nonce.hex()
+  system.config["configHash"] = sha256(sha256(static).digest()).hexdigest()
+  with open("./system.config.json", "w") as w:
+    json.dump(system.config, w, indent=4)
 
 def verify():
   verified = False
-  if config["configHash"] == sha256(sha256(static).digest()).hexdigest():
+  if system.config["configHash"] == sha256(sha256(static).digest()).hexdigest():
     verified = True
   return verified
