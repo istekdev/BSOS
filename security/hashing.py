276f6c5c7ba68a52ee7045d8b4d530ea9c0e41f1
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-09 19:54:49 -0600

    Update hashing.py

--- security/hashing.py
+++ security/hashing.py
@@ -5,7 +5,7 @@ import os
 
 with open("config.json", "r") as r:
   config = json.load(r)
-static = ((config["socials"]["developer"]).encode("utf-8") + (config["socials"]["github"]).encode("utf-8") + str(config["socials"]["created"]).encode("utf-8") + str(config["config"]["version"]).encode("utf-8") + str(config["config"]["colors"]).encode("utf-8") + (config["dynamic"]["user"]).encode("utf-8") + bytes.fromhex(config["dynamic"]["nonce"]) + str(config["apps"]["bank"]["balance"]).encode("utf-8"))
+static = ((config["socials"]["developer"]).encode("utf-8") + (config["socials"]["github"]).encode("utf-8") + str(config["socials"]["created"]).encode("utf-8") + str(config["config"]["version"]).encode("utf-8") + str(config["config"]["colors"]).encode("utf-8") + (config["dynamic"]["user"]).encode("utf-8") + bytes.fromhex(config["dynamic"]["salt"]) + bytes.fromhex(config["dynamic"]["nonce"]) + str(config["apps"]["bank"]["balance"]).encode("utf-8"))
 
 def start():
   if config["dynamic"]["nonce"] == None:
