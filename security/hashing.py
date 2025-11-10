794b98496f38daf042cc0a515c7ebfdfcdd15e77
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-10 10:47:50 -0600

    Update hashing.py

--- security/hashing.py
+++ security/hashing.py
@@ -11,7 +11,7 @@ if config["dynamic"]["timestamp"] == None:
   timestamp = "null"
 else:
   timestamp = config["dynamic"]["timestamp"]
-static = ((config["socials"]["developer"]).encode("utf-8") + (config["socials"]["github"]).encode("utf-8") + str(config["socials"]["created"]).encode("utf-8") + str(config["config"]["version"]).encode("utf-8") + str(config["config"]["colors"]).encode("utf-8") + (config["dynamic"]["user"]).encode("utf-8") + str(config["dynamic"]["timestamp"]).encode("utf-8") + bytes.fromhex(config["dynamic"]["salt"]) + bytes.fromhex(config["dynamic"]["nonce"]) + str(config["apps"]["bank"]["balance"]).encode("utf-8"))
+static = ((config["socials"]["developer"]).encode("utf-8") + (config["socials"]["github"]).encode("utf-8") + str(config["socials"]["created"]).encode("utf-8") + str(config["config"]["version"]).encode("utf-8") + str(config["config"]["colors"]).encode("utf-8") + (config["dynamic"]["user"]).encode("utf-8") + (config["dynamic"]["status"]).encode("utf-8") + str(config["dynamic"]["timestamp"]).encode("utf-8") + bytes.fromhex(config["dynamic"]["salt"]) + bytes.fromhex(config["dynamic"]["nonce"]) + str(config["apps"]["bank"]["balance"]).encode("utf-8"))
 
 def start():
   if config["dynamic"]["nonce"] == None:
